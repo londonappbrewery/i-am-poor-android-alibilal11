afe30b910fa87d38c8c21f849a59176736d6580a
LENOVO <alibilal11>
2018-08-30 15:55:58 +0500

    Challenge Completed

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -1,8 +1,11 @@
 package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void browser1(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com.pk") );
+        startActivity(browserIntent);
+
     }
 }
